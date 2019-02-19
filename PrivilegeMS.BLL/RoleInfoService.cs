a5275fe0e5d5b1ecdc0d4639a87631f2f972304d
ruisenLi <ruisenLi@163.com>
2019-02-20 00:43:14 +0800

    完成角色权限分配，但是出现了双向引用BUG

---
+++ PrivilegeMS.BLL/RoleInfoService.cs
@@ -0,0 +1,32 @@
+﻿using PrivilegeMS.IBLL;
+using PrivilegeMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivilegeMS.BLL
+{
+    public partial class RoleInfoService : BaseService<RoleInfo>, IRoleInfoService
+    {
+       public  bool SetRoleActionInfo(int roleId, List<int> actionIdList)
+        {
+            var roleInfo = this.LoadEntities(r => r.ID == roleId && r.DelFlag == true).FirstOrDefault();
+            if (roleInfo!=null)
+            {
+                roleInfo.ActionInfo.Clear();
+                foreach (int actionId in actionIdList)
+                {
+                    var actionInfo = this.CurrentDBSession.ActionInfoDal.LoadEntities(a => a.ID == actionId && a.DelFlag == true).FirstOrDefault();
+                    if (actionInfo!=null)
+                    {
+                        roleInfo.ActionInfo.Add(actionInfo);
+                    }
+                }
+                return this.CurrentDBSession.SaveChanges();
+            }
+            return false;
+        }
+    }
+}
--- PrivilegeMS.WebAPP/Controllers/RoleController.cs
+++ PrivilegeMS.WebAPP/Controllers/RoleController.cs
@@ -3,15 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PrivilegeMS.IBLL;
+using PrivilegeMS.BLL;
 namespace PrivilegeMS.WebAPP.Controllers
 {
     public class RoleController : Controller
     {
+        IBLL.IRoleInfoService roleInfoService = new BLL.RoleInfoService();
+        IBLL.IActionInfoService ActionInfoService = new BLL.ActionInfoService();
         // GET: Role
         public ActionResult Index()
         {
             return View();
+        }
+        //GET 获取角色列表
+        [HttpGet]
+        public ActionResult RoleList()
+        {
+            var role = roleInfoService.LoadEntities(r => r.DelFlag == true);
+            return Json(role, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult AddRole(string name,string remark,int sort)
+        {
+            Model.RoleInfo roleInfo = new Model.RoleInfo();
+            roleInfo.Remark = remark;
+            roleInfo.Sort = sort;
+            roleInfo.Name = name;
+            roleInfo.SubTime = DateTime.Now;
+            roleInfo.DelFlag = true;
+            var role = roleInfoService.AddEntity(roleInfo);
+            if (role.ID!=0&&role!=null)
+            {
+                return Content("ok");
+            }
+            return Content("no");
+        }
+        [HttpPost]
+        public ActionResult DeleteRole(int id)
+        {
+            var roleinfo = roleInfoService.LoadEntities(r => r.ID == id && r.DelFlag == true).FirstOrDefault();
+            if (roleinfo!=null)
+            {
+                roleinfo.DelFlag = false;
+                var flag = roleInfoService.EditEntity(roleinfo);
+                if (flag)
+                {
+                    return Content("ok");
+                }
+            }
+            return Content("no");
+        }
+        [HttpPost]
+        public ActionResult EditRole(string name,string remark,int id,int sort)
+        {
+            var roleInfo = roleInfoService.LoadEntities(r => r.ID == id && r.DelFlag == true).FirstOrDefault();
+            if (roleInfo!=null)
+            {
+                roleInfo.Name = name;
+                roleInfo.Remark = remark;
+                roleInfo.Sort = sort;
+                roleInfo.ModifiedTime = DateTime.Now;
+                var flag = roleInfoService.EditEntity(roleInfo);
+                if (flag)
+                {
+                    return Content("ok");
+                }
+            }
+            return Content("no");
+        }
+        public ActionResult Actioninfo(int id)
+        {
+            var roleinfo = roleInfoService.LoadEntities(r => r.ID == id && r.DelFlag == true).FirstOrDefault();
+            if (roleinfo != null)
+            {
+                var roleInfoAction = (from a in roleinfo.ActionInfo
+                                      select a.ID).ToList();
+                var actioninfo = ActionInfoService.LoadEntities(a => a.DelFlag == true);
+                //var actioninfo = ActionInfoService.LoadEntities(a => a.DelFlag == true && !roleInfoAction.Contains(a.ID)).FirstOrDefault();
+                return Json(actioninfo, JsonRequestBehavior.AllowGet);
+            }
+            return Content("no");
+            //return Content("no");
+            //var actioninfo = ActionInfoService.LoadEntities(a => a.DelFlag == true);
+            //return Json(actioninfo, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult SetRoleAction(int id,string idList)
+        {
+            string[] IdListS = idList.Substring(1, idList.Length - 2).Split(',');
+            List<int> IdList = new List<int>();
+            foreach (var item in IdListS)
+            {
+                IdList.Add(Convert.ToInt32(item));
+            }
+            if (roleInfoService.SetRoleActionInfo(id,IdList))
+            {
+                return Content("ok");
+            }
+            return Content("no");
         }
     }
 }
